1ba0b78825d7a906059da0bffc8848e0a53b232e
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-09-22 23:49:08 +0530

    New Project

---
+++ Part1/script.js
@@ -0,0 +1,17 @@
+gsap.to("#box1" ,{
+    x : 1000,
+    
+    duration: 2,
+    delay: 1,
+    rotate : 360,
+    backgroundColor:"blue",
+    boderRadius:"50%",})
+
+ gsap.from("#box2" ,{
+    x : 1200,
+  
+    duration : 2,
+    delay: 1,
+    backgroundColor:"White",
+    boderRadius:"50%"
+ })
