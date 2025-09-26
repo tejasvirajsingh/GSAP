f0bee7783172118badae5cfb99df88f67c50c525
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-09-27 04:37:08 +0530

    Use Y axis

--- Part1/script.js
+++ Part1/script.js
@@ -7,13 +7,12 @@ gsap.to("#box1" ,{
     backgroundColor:"blue",
     boderRadius:"50%",})
 
- gsap.from("#box2" ,{
+ gsap.to("#box2" ,{
     x : 1200,
-  
+    y : 300,
     duration : 2,
     delay: 1,
     backgroundColor:"White",
     boderRadius:"50%"
  })
 
- 
