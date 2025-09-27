ed145e564f4f66eecdb6826408aa7aed699fc5f3
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-09-27 23:01:49 +0530

    Use new function

--- Part1/script.js
+++ Part1/script.js
@@ -3,13 +3,13 @@ gsap.to("#box1" ,{
     
     duration: 2,
     delay: 1,
-    rotate : 360,
+    rotate : 90,
     backgroundColor:"blue",
     boderRadius:"50%",})
 
  gsap.to("#box2" ,{
-    x : 1200,
-    y : 300,
+    x : 1000,
+    y : 50,
     duration : 2,
     delay: 1,
     backgroundColor:"White",
