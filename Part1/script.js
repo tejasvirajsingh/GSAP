000d2a5bfd73b74b4632e716fcad5b92d9e5a6de
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-11 23:41:36 +0530

    update the box 3

--- Part1/script.js
+++ Part1/script.js
@@ -1,18 +1,25 @@
 gsap.to("#box1" ,{
-    x : 700,
+    x : 1200,
     
-    duration: 2,
+    duration: 1,
     delay: 1,
     rotate : 90,
     backgroundColor:"blue",
     boderRadius:"50%",})
 
  gsap.to("#box2" ,{
-    x : 700,
-    y : 50,
+    x : 1200,
     duration : 2,
     delay: 1,
     backgroundColor:"White",
     boderRadius:"50%"
  })
-
+gsap.to("#box3", {
+   x:1200,
+   duration:3,
+   delay:1,
+   rotate:180,
+   backgroundColor:"Red",
+   boderRadius:"50%",
+   
+})
