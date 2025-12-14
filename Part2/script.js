c2f81eaa57355925ee450a32cae1794026366930
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-15 00:22:28 +0530

    link add of Trigger scrolling

--- Part2/script.js
+++ Part2/script.js
@@ -1,23 +1,23 @@
 gsap.from("#page1 #box",{
     scale:0,
     delay:1,
-    duration:1,
+    duration:2,
     rotate:180
 
 })
 
 gsap.from("#page2 #box",{
     scale:0,
-    delay:1,
-    duration:4,
+    delay:3,
+    duration:2,
     rotate:180
 
 })
 
 gsap.from("#page3 #box",{
     scale:0,
-    delay:1,
-    duration:6,
+    delay:5,
+    duration:1,
     rotate:180
 
 })
