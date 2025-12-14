1b8bfb7c38eacf1ef319bd12712e604318e419b3
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-14 23:56:21 +0530

    Make it style and function

---
+++ Part2/script.js
@@ -0,0 +1,23 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:180
+
+})
+
+gsap.from("#page2 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:180
+
+})
+
+gsap.from("#page3 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:180
+
+})
